976ab304eb9e4ef6c2435c412dd93f14b4870015
iwasaku <hgh02676+github@gmail.com>
2021-04-29 22:55:19 +0900

    ロゴの文字列を修正 ロゴの表示位置を修正

--- KMT/main.js
+++ KMT/main.js
@@ -792,11 +792,11 @@ tm.define("LogoScene", {
                 {
                     type: "Label", name: "logoLabel",
                     x: SCREEN_CENTER_X,
-                    y: 320,
+                    y: SCREEN_CENTER_Y,
                     fillStyle: "#888",
                     fontSize: 64,
                     fontFamily: FONT_FAMILY,
-                    text: "LOGO",
+                    text: "UNOFFICIAL GAME",
                     align: "center",
                 },
             ]
@@ -806,7 +806,7 @@ tm.define("LogoScene", {
 
     update: function (app) {
         // 時間が来たらタイトルへ
-        //        if(++this.localTimer >= 5*app.fps)
+        //if (++this.localTimer >= 5 * app.fps)
         this.app.replaceScene(TitleScene());
     }
 });
